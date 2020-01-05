768b0ac7bb87bcf432c3e7080ca7c39555d31e2a
CalWal <callumtrwallace@gmail.com>
2020-01-05 15:02:04 +1000

    basic component setup

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import UserCreate from "./UserCreate";
 
 class App extends Component {
+  state = {
+    language: "english"
+  };
+
+  onLanguageChange = language => {
+    this.setState({
+      language: language
+    });
+  };
+
   render() {
-    return <div className="ui container">Hello World</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            onClick={() => this.onLanguageChange("english")}
+            className="au flag"
+          />
+          <i
+            onClick={() => this.onLanguageChange("german")}
+            className="de flag"
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
   }
 }
 
